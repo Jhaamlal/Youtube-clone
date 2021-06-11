f034c31ce321d4a9aaffddd879a48c9d63cb225b
ravi shankar <shankarji1729@gmail.com>
2021-06-11 10:24:20 +0530

    Initialisation of app

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZxT4-p5B6FBUQb09Fu-WSQNaxLwbp3I4",
+    authDomain: "clone-c90f1.firebaseapp.com",
+    projectId: "clone-c90f1",
+    storageBucket: "clone-c90f1.appspot.com",
+    messagingSenderId: "572241835432",
+    appId: "1:572241835432:web:4a58497761b3fd7e17599d"
+};
